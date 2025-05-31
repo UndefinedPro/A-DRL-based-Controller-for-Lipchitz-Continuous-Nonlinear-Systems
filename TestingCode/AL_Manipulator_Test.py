f2812f31cbd0a6181ccd8b1a9265a90caf3cf3fb
UndefinedPro <58689660+UndefinedPro@users.noreply.github.com>
2025-05-31 16:08:13 +0800

    Delete TestingCode directory

--- TestingCode/AL_Manipulator_Test.py
+++
@@ -1,8 +0,0 @@
-from Train import TRAIN_AL_MANIPULATOR
-
-
-netname = 'Lyp'
-t = TRAIN_AL_MANIPULATOR(netname, 2000)
-
-path = './param/AL/Manipulator_param_in_paper/' + netname + 'actorNet0.pkl' 
-t.Test(path)
--- TestingCode/AL_Spring _Test.py
+++
@@ -1,9 +0,0 @@
-from Train import TRAIN_AL_SPRING
-
-
-netname = 'Lyp'
-t = TRAIN_AL_SPRING(netname, 2000)
-
-for i in range(10):
-    path = './param/AL/Spring_param_in_paper/' + netname + 'actorNet' + str(i) + '.pkl' 
-    t.Test(path)
